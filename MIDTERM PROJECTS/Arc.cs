601a6ce996a3f1b6e7d66d33d3aac74434fad34b
Tungdever <sidedlove03@gmail.com>
2023-04-05 00:54:06 +0700

    final1

--- MIDTERM PROJECTS/Arc.cs
+++ MIDTERM PROJECTS/Arc.cs
@@ -21,7 +21,13 @@ namespace MIDTERM_PROJECTS
             int radius = (int)Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2)) / 2;
             double startAngle = Math.Atan2(p1.Y - centerY, p1.X - centerX) * 180 / Math.PI;
             double endAngle = Math.Atan2(p2.Y - centerY, p2.X - centerX) * 180 / Math.PI;
-            gp.DrawArc(myPen, centerX - radius, centerY - radius, radius * 2, radius * 2, (float)startAngle, (float)(endAngle - startAngle));
+            try
+            {
+                gp.DrawArc(myPen, centerX - radius, centerY - radius, radius * 2, radius * 2, (float)startAngle, (float)(endAngle - startAngle));
+            }
+            catch 
+            { 
+            }
         }
 
         public override void Move(int deltaX, int deltaY)
